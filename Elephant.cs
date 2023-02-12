c4cb6c3864d1402b587f69d14a0ebb77df51fff2
Антон Самарский <anton.samarskii.97@gmail.com>
2023-02-12 11:06:11 +0100

    Add project files.

---
+++ Elephant.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elephant
+{
+    internal class Elephant
+    {
+        public int EarSize;
+        public string Name;
+        public void WhoAmI()
+        {
+            Console.WriteLine("My name is " + Name + ".");
+            Console.WriteLine("My ears are " + EarSize + " inches tall. ");
+        }
+
+        public void HearMassage(string message, Elephant whoSaidIt)
+        {
+            Console.WriteLine(Name + " heard a massage ");
+            Console.WriteLine(whoSaidIt.Name + " said this: " + message);
+
+        }
+
+        public void SpeakTo(Elephant whoToTalkTo, string message)
+        {
+            whoToTalkTo.HearMassage(message, this);
+        }
+
+    }
+}
